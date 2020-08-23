b96493dfb6fa5010e67e104e9baca5a5e06373ab
atomicfruitcake <sam@wirewax.com>
2020-08-23 19:32:49 +0100

    Get ready for pypi

--- __init__.py
+++ __init__.py
@@ -3,4 +3,8 @@
 
 @date 2020
 """
-__version__ = "1.0.0"
+__version__ = "0.0.1"
+
+from srv.gnutty import Gnutty
+from srv.responses import response, response_codes, not_authorized_response
+from srv import request, request_methods
--- srv/gnutty.py
+++ srv/gnutty.py
@@ -10,8 +10,6 @@ from pathlib import Path
 
 from srv import constants
 from srv.gnuttycore import GnuttyCore
-from srv.responses.response import Response
-from srv.responses.response_codes import ResponseCodes
 
 
 class Gnutty(GnuttyCore):
@@ -35,44 +33,3 @@ class Gnutty(GnuttyCore):
             if not favicon
             else favicon
         )
-
-
-def run_gnutty():
-    server = Gnutty(port=8000)
-
-    @server.get("/")
-    def root(request):
-        return Response(body="OK")
-
-    @server.get("/favicon.ico")
-    def favicon(request):
-        return Response(
-            body=open(
-                os.path.join(
-                    Path(os.path.dirname(__file__)).parent,
-                    "favicon.ico"
-                ),
-                "rb",
-            ).read()
-        )
-
-    @server.post("/test", auth="basic")
-    def new(request):
-        return Response(
-            body=request.body,
-            content_type="application/json",
-        )
-
-    @server.any()
-    def not_found(request):
-        return Response(
-            code=ResponseCodes.NOT_FOUND.value,
-            body="NOT FOUND",
-            content_type="text/html",
-        )
-
-    server.serve()
-
-
-if __name__ == "__main__":
-    run_gnutty()
